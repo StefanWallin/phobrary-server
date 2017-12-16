a49f2c9d81a6cc2e3b9cc8fb4a525ef347dfa495
Stefan Wallin <stefan.w@festiz.com>
2017-12-16 17:19:05 +0100

    Improve stats overview

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,6 +3,14 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all
+    date_sorted_shots = Shot.all.order(:date)
+    @n_photos = Photo.count
+    @n_shots = Shot.count
+    @n_cameras = Camera.count
+    shot_photos = date_sorted_shots.map do |s|
+      s.photos
+    end
+    @photos_per_shot = date_sorted_shots.map { |s| { shot: s.id, photos: s.photos.length } }.sort{ |x,y| y[:photos] <=> x[:photos] }.take 5
+    @photos = shot_photos.flatten
   end
 end
