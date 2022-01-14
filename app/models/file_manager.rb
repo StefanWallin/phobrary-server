35ba0fa5b346a17b7be8f618de63b2bb96f3b07a
Stefan Wallin <github@stefan-wallin.se>
2022-01-14 23:13:07 +0100

    Add where we are in debugging for next time

--- app/models/file_manager.rb
+++ app/models/file_manager.rb
@@ -4,6 +4,7 @@ require 'fileutils'
 class FileManager
   LIBRARY_DIR = File.join('..', 'library', 'target')
   def self.move_uploaded_file(uid, filename, username)
+    # TODO, schedule this async and see if file lock on the info-file is released.
     uploaddir = File.join(LIBRARY_DIR, 'uploads', username)
     FileUtils.mkdir_p uploaddir
     current_path = File.join('.', 'public', 'tus', uid)
@@ -11,6 +12,7 @@ class FileManager
     FileUtils.mv current_path, target_path
     info_path = File.join('.', 'public', 'tus', "#{uid}.info")
     puts "REMOVING: #{info_path} - #{File.exists?(info_path)}"
+    puts exec('lsof')
     File.delete(info_path)
     puts "#{LIBRARY_DIR}"
     puts "#{uploaddir}"
